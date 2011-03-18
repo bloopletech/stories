35f2ea501356866ac8201e9fe7ce35532e1aa67f
Brenton 'B-Train' Fletcher <i@bloople.net>
2011-03-18 14:07:16 +1100

    Fix file santizing

--- lib/file_extensions.rb
+++ lib/file_extensions.rb
@@ -1,11 +1,11 @@
 class File
   BAD_CHARACTER_REGEX = /([ \[\]\(\)'"&!\\])/
-
+  
   def self.escape_name(filename)
     filename.gsub(BAD_CHARACTER_REGEX) { |r| "\\#{$1}" }
   end
 
   def self.sanitize_name(filename)
-    filename.gsub(BAD_CHARACTER_REGEX, "_")
+    filename.gsub('/', '_').gsub(/[[:space:]]+/, ' ').gsub(BAD_CHARACTER_REGEX, "_")
   end
 end
