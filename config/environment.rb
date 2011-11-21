8af6037e351f3e1bce77b09ae2c72ee6fb6b3b23
Brenton Fletcher <brenton@coupa.com>
2011-11-21 22:25:43 +1100

    Fix for daemon

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,9 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Stories::Application.initialize!
 
-ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :pool => 5, :timeout => 5000, :database => "#{Stories.dir}/db.sqlite3")    
+db_config = { :adapter => 'sqlite3', :pool => 5, :timeout => 5000, :database => "#{Stories.dir}/db.sqlite3" }
+ActiveRecord::Base.establish_connection(db_config)
 ActiveRecord::Migrator.migrate("db/migrate/")
+ActiveRecord::Base.establish_connection(db_config)
 
 #%w(open gnome-open).detect { |app| system("#{app} http://localhost:30813/") } unless $0 =~ /^rake|irb$/
