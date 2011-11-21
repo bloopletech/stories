d5da1a8f5807667db8b4ab50d8ceb527bc147dca
Brenton Fletcher <brenton@coupa.com>
2011-11-21 22:38:39 +1100

    Fixes for production

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Stories::Application.initialize!
 
-db_config = { :adapter => 'sqlite3', :pool => 5, :timeout => 5000, :database => "#{Stories.dir}/db.sqlite3" }
-ActiveRecord::Base.establish_connection(db_config)
-ActiveRecord::Migrator.migrate("db/migrate/")
-ActiveRecord::Base.establish_connection(db_config)
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :pool => 5, :timeout => 5000, :database => "#{Stories.dir}/db.sqlite3")
+ActiveRecord::Migrator.migrate("#{Rails.root}/db/migrate/")
 
 #%w(open gnome-open).detect { |app| system("#{app} http://localhost:30813/") } unless $0 =~ /^rake|irb$/
