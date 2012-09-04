bc7cae049980d74dc26008690b00a9d74860b782
Brenton 'B-Train' Fletcher <i@bloople.net>
2012-09-04 18:11:55 +1000

    Fix stylesheet; fix search form

--- public/javascripts/story-index.js
+++ public/javascripts/story-index.js
@@ -5,3 +5,12 @@ window.onload = function() {
   document.getElementById("sort").onchange = submit_func;
   document.getElementById("sort_direction").onchange = submit_func;
 };
+
+$(function() {
+  $("#search_form").click(function(event) {
+    event.stopPropagation();
+  }).keydown(function(event)
+  {
+    if(event.keyCode == 32 || event.keyCode == 8) event.stopPropagation();
+  });
+});
